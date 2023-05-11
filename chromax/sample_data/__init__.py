5ca442a75c8d2344bfe6fa04617cbe7b2f533996
Omar Younis <omar.younis98@gmail.com>
2023-05-11 16:33:38 +0200

    fix genome file path

--- chromax/sample_data/__init__.py
+++ chromax/sample_data/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 genetic_map = Path(__file__).parent.joinpath("genetic_map.csv")
-genome = Path(__file__).parent.joinpath("genome.txt")
+genome = Path(__file__).parent.joinpath("genome.npy")
