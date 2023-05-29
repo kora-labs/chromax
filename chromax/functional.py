df357501526d3d6fb35ae9273d67b25ca5fc0e79
Omar Younis <omar.younis98@gmail.com>
2023-05-29 13:37:15 +0200

    update functional doc

--- chromax/functional.py
+++ chromax/functional.py
@@ -13,32 +13,34 @@ def cross(
 ) -> Population["n"]:
     """Main function that computes crosses from a list of parents.
 
-    Args:
-        - parents (array): parents to compute the cross. The shape of
+    :param parents: parents to compute the cross. The shape of
         the parents is (n, 2, m, d), where n is the number of parents, 
         m is the number of markers, and d is the ploidy.
-        - recombination_vec (array): array of m probabilities.
+    :type parents: ndarray
+    :param recombination_vec: array of m probabilities.
         The i-th value represent the probability to recombine before the marker i.
-        - random_key (array): PRNGKey, for reproducibility purpose
+    :type recombination_vec:
+    :param random_key: JAX PRNGKey, for reproducibility purpose.
+    :type random_key: jax.random.PRNGKeyArray
 
-    Returns:
-        - population (array): offspring population of shape (n, m, d).
+    :return: offspring population of shape (n, m, d).
+    :rtype: ndarray
 
-    Example:
-    >>> from chromax import functional
-    >>> import numpy as np
-    >>> import jax
-    >>> n_chr, chr_len, ploidy = 10, 100, 2
-    >>> n_crosses = 50
-    >>> parents_shape = (n_crosses, 2, n_chr * chr_len, ploidy)
-    >>> parents = np.random.choice([False, True], size=parents_shape)
-    >>> rec_vec = np.full((n_chr, chr_len), 1.5 / chr_len)
-    >>> rec_vec[:, 0] = 0.5  # equal probability on starting haploid
-    >>> rec_vec = rec_vec.flatten()
-    >>> random_key = jax.random.PRNGKey(42)
-    >>> f2 = functional.cross(parents, rec_vec, random_key)
-    >>> f2.shape
-    (50, 1000, 2)
+    :Example:
+        >>> from chromax import functional
+        >>> import numpy as np
+        >>> import jax
+        >>> n_chr, chr_len, ploidy = 10, 100, 2
+        >>> n_crosses = 50
+        >>> parents_shape = (n_crosses, 2, n_chr * chr_len, ploidy)
+        >>> parents = np.random.choice([False, True], size=parents_shape)
+        >>> rec_vec = np.full((n_chr, chr_len), 1.5 / chr_len)
+        >>> rec_vec[:, 0] = 0.5  # equal probability on starting haploid
+        >>> rec_vec = rec_vec.flatten()
+        >>> random_key = jax.random.PRNGKey(42)
+        >>> f2 = functional.cross(parents, rec_vec, random_key)
+        >>> f2.shape
+        (50, 1000, 2)
     """
     parents = parents.reshape(*parents.shape[:3], -1, 2)
     random_keys = jax.random.split(random_key, num=len(parents) * 2 * parents.shape[3])
@@ -70,31 +72,34 @@ def double_haploid(
 ) -> Population["n n_offspring"]:
     """Computes the double haploid of the input population.
 
-    Args:
-        - population (array): input population of shape (n, m, d).
-        - n_offspring (int): number of offspring per plant.
-        - recombination_vec (array): array of m probabilities.
+    :param population: input population of shape (n, m, d).
+    :type population: ndarray
+    :param n_offspring: number of offspring per plant.
+    :type n_offspring: int
+    :param recombination_vec: array of m probabilities.
         The i-th value represent the probability to recombine before the marker i.
-        - random_key (array): array of n PRNGKey, one for each individual.
+    :type recombination_vec: ndarray
+    :param random_key: array of n PRNGKey, one for each individual.
+    :type random_key: jax.random.PRNGKeyArray
 
-    Returns:
-        - population (array): output population of shape (n, n_offspring, m, d).
+    :return: output population of shape (n, n_offspring, m, d).
         This population will be homozygote.
+    :rtype: ndarray
     
-    Example:
-    >>> from chromax import functional
-    >>> import numpy as np
-    >>> import jax
-    >>> n_chr, chr_len, ploidy = 10, 100, 2
-    >>> pop_shape = (50, n_chr * chr_len, ploidy)
-    >>> f1 = np.random.choice([False, True], size=pop_shape)
-    >>> rec_vec = np.full((n_chr, chr_len), 1.5 / chr_len)
-    >>> rec_vec[:, 0] = 0.5  # equal probability on starting haploid
-    >>> rec_vec = rec_vec.flatten()
-    >>> random_key = jax.random.PRNGKey(42)
-    >>> dh = functional.double_haploid(f1, 10, rec_vec, random_key)
-    >>> dh.shape
-    (50, 10, 1000, 2)
+    :Example:
+        >>> from chromax import functional
+        >>> import numpy as np
+        >>> import jax
+        >>> n_chr, chr_len, ploidy = 10, 100, 2
+        >>> pop_shape = (50, n_chr * chr_len, ploidy)
+        >>> f1 = np.random.choice([False, True], size=pop_shape)
+        >>> rec_vec = np.full((n_chr, chr_len), 1.5 / chr_len)
+        >>> rec_vec[:, 0] = 0.5  # equal probability on starting haploid
+        >>> rec_vec = rec_vec.flatten()
+        >>> random_key = jax.random.PRNGKey(42)
+        >>> dh = functional.double_haploid(f1, 10, rec_vec, random_key)
+        >>> dh.shape
+        (50, 10, 1000, 2)
     """
     population = population.reshape(*population.shape[:2], -1, 2)
     keys = jax.random.split(
@@ -149,30 +154,32 @@ def select(
 ) -> Population["k"]:
     """Function to select individuals based on their score (index).
 
-    Args:
-        - population (array): input grouped population of shape (n, m, d)
-        - k (int): number of individual to select.
-        - f_index (function): function that computes a score for each individual.
+    :param population: input grouped population of shape (n, m, d)
+    :type population: ndarray
+    :param k: number of individual to select.
+    :type k: int
+    :param f_index: function that computes a score for each individual.
         The function accepts as input a population, i.e. and array of shape
         (n, m, 2) and returns an arrray of n float number.
+    :type f_index: Callable
 
-    Returns:
-        - population (array): output population of (k, m, d)
+    :return: output population of (k, m, d)
+    :rtype: ndarray
 
-    Example:
-    >>> from chromax import functional
-    >>> from chromax.trait_model import TraitModel
-    >>> from chromax.index_functions import conventional_index
-    >>> import numpy as np
-    >>> n_chr, chr_len, ploidy = 10, 100, 2
-    >>> pop_shape = (50, n_chr * chr_len, ploidy)
-    >>> f1 = np.random.choice([False, True], size=pop_shape)
-    >>> marker_effects = np.random.randn(n_chr * chr_len)
-    >>> gebv_model = TraitModel(marker_effects[:, None])
-    >>> f_index = conventional_index(gebv_model)
-    >>> f2 = functional.select(f1, k=10, f_index=f_index)
-    >>> f2.shape
-    (10, 1000, 2)
+    :Example:
+        >>> from chromax import functional
+        >>> from chromax.trait_model import TraitModel
+        >>> from chromax.index_functions import conventional_index
+        >>> import numpy as np
+        >>> n_chr, chr_len, ploidy = 10, 100, 2
+        >>> pop_shape = (50, n_chr * chr_len, ploidy)
+        >>> f1 = np.random.choice([False, True], size=pop_shape)
+        >>> marker_effects = np.random.randn(n_chr * chr_len)
+        >>> gebv_model = TraitModel(marker_effects[:, None])
+        >>> f_index = conventional_index(gebv_model)
+        >>> f2 = functional.select(f1, k=10, f_index=f_index)
+        >>> f2.shape
+        (10, 1000, 2)
     """
 
     indices = f_index(population)
