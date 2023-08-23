5b6e51b5f70898fc4c8d0e8e7de525ede773e637
Omar Younis <omar.younis98@gmail.com>
2023-08-23 15:16:09 +0200

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-on_rtd = os.environ.get("READTHEDOCS") == "True"
-if on_rtd:
-    html_theme = "default"
-else:
-    html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 html_static_path = ['_static']
 autodoc_member_order = 'bysource'
