53ca4276ffd0fc3c924cd60ece077d27472679f4
zhanglei <zhanglei@kingagroot.com>
2024-01-30 16:19:04 +0800

    mutate code fix

--- chromax/simulator.py
+++ chromax/simulator.py
@@ -236,8 +236,8 @@ class Simulator:
             >>> f2.shape
             (3, 9839, 2)
         """
-        self.random_key, random_key = jax.random.split(self.random_key)
-        return functional.cross(parents, self.recombination_vec, random_key, self.mutation)
+        self.random_key, split_key = jax.random.split(self.random_key)
+        return functional.cross(parents, self.recombination_vec, split_key, self.mutation)
 
     @property
     def differentiable_cross_func(self) -> Callable:
@@ -315,9 +315,9 @@ class Simulator:
             >>> dh.shape
             (371, 10, 9839, 2)
         """
-        self.random_key, random_key = jax.random.split(self.random_key)
+        self.random_key, split_key = jax.random.split(self.random_key)
         dh = functional.double_haploid(
-            population, n_offspring, self.recombination_vec, random_key, self.mutation
+            population, n_offspring, self.recombination_vec, split_key, self.mutation
         )
 
         if n_offspring == 1:
